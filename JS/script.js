949fc9d38e8e35c2673264828444c9f18cce0b38
naveen <naveenkgowda2612@gmail.com>
2025-04-03 18:42:21 +0530

    add new HTML, JavaScript, and JSON files for various functionalities and examples

---
+++ JS/script.js
@@ -0,0 +1,77 @@
+// let buttons = document.querySelector("button11");
+// console.log(buttons);
+// button[0].addEventListener('click',() => {
+//     console.log("button clicked");
+
+// })
+// for(i = 0;i < button.length ;i++){
+//     console.log(button[i]);
+//     button[i].addEventListener('click',() => {
+//         console.log("button");
+//     })
+
+// }
+// buttons.forEach((currentButton) => {
+//     currentButton.addEventListener('click',() => {
+//         console.log("button");
+//     })
+// });
+
+// Functions declaration and expression
+// myFunction1();
+
+// function myFunction1() {
+//     console.log("My function 1 is called");
+// }
+
+// const myFunction2 = () => {
+//     console.log("My function 2 is called");
+// }
+// myFunction2();
+
+
+// operators 
+// let personAge = 20;
+
+// const a = 30;
+
+// a %= 7;
+
+// let a = 10;
+// let b = 20;
+
+// let c = a + b;
+
+// console.log(a);
+
+// let x = (100 + 50) * a;
+// let x = 100 + 50 * a;
+// console.log(x);
+
+// let c = 10;
+
+
+// c**5;
+// // c = c - 1;
+// console.log(c);
+
+// let isTrue = (2 == 2);
+// console.log(isTrue);
+
+// let a = 12;
+// let c = 19;
+// let b = "bhjgj";
+
+// console.log(b + a + c);
+
+// let isTrue = (2 == 4) && (3 == 1);
+// console.log(isTrue);
+
+// let isTrue = (2 == 4) || (1 == 1);
+// let isTrue = !true;
+// console.log(isTrue);
+
+function myFunction() {
+    console.log("My function is called");
+}
+myFunction();
