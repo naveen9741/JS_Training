6f6fb36a8d55fd96b6293eb9bf98da6a2813da4d
naveen <naveenkgowda2612@gmail.com>
2025-04-04 12:05:08 +0530

    add new JavaScript files with example functions and truthy value checks

---
+++ JS/functions.js
@@ -0,0 +1,41 @@
+// const myFunction = (parameter1 = "default parameter") => {
+//     console.log("My function is called!", parameter1);
+// }
+// myFunction("My parameter!");
+// myFunction();
+
+// function myFunction2() {
+//     console.log("My function 2 is called!");
+// }
+// myFunction2();
+
+// syntax
+// functionName()
+
+// const sum = (a = 0, b = 0, c = 0) => {
+//     return a + b + c;
+// }
+
+// const sum = function (a = 0, b = 0, c = 0) {
+//     return a + b + c;
+// }
+
+// console.log(sum(2,66,9));
+
+
+// const sum = (a, b) => {
+//     return (a + b);
+// }
+
+// const getFunctionValue = sum(44,33);
+// console.log(getFunctionValue);
+
+// let valueA = 44;
+// let valueB = 33;
+
+// const multiply = (a, b) => {
+//     return a * b;
+// }
+
+// const getFunctionValue = multiply(valueA, valueB);
+// console.log(getFunctionValue);
---
+++ JS/truthyValues.js
@@ -0,0 +1,25 @@
+// let a = null;
+
+// if (a) {
+//     console.log("True!");
+// } else {
+//     console.log("False!");
+// }
+
+// !a && console.log("False!");
+
+// if () {
+
+// }else if(){
+
+// }else if(){
+
+// }else{
+
+// }
+
+let name = "John*Doe*gfhj*gfhfghf";
+
+let slicedName = name.slice(4, 10);
+// console.log(name.split("*"));
+console.log(slicedName);
